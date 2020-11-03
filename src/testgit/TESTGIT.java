8c8d1032e964b3b5de148cb8eb61c92b2ee41c91
Phunon <phunon.sriv@mail.kmutt.ac.th>
2020-11-03 21:25:02 +0700

    HelloDel

--- src/testgit/TESTGIT.java
+++ src/testgit/TESTGIT.java
@@ -17,7 +17,7 @@ public class TESTGIT {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
-        System.out.println("Hello2");
+       
     }
     
 }
